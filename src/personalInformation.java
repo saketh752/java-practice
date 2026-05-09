70ce0c5c162beaa62bed9220c5238a4da89940be
saketh752 <sakethyadav208@gmail.com>
2026-05-10 00:36:37 +0530

    Fixed Java class and file names

--- src/personalInformation.java
+++ src/personalInformation.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class personalInformation {
+public class PersonalInformation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your name: ");
