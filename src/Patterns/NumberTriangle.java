15b7f8ce2b1b3ac307af3a5a4d30a2dd12daacce
saketh752 <sakethyadav208@gmail.com>
2026-05-15 23:12:52 +0530

    Pattrens Practiced

---
+++ src/Patterns/NumberTriangle.java
@@ -0,0 +1,15 @@
+package Patterns;
+import java.util.Scanner;
+public class NumberTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i<=4; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(n + " ");
+                n++;
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/Patterns/SquarePattern.java
@@ -0,0 +1,12 @@
+package Patterns;
+
+public class SquarePattern {
+    public static void main(String[] args) {
+        for(int i = 1; i <=3; i++){
+            for(int j = 1; j <=3; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
