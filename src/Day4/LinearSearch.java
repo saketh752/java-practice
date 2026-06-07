564c2bbbc2ad0458cd72a78f6397ce127152fa7a
saketh752 <sakethyadav208@gmail.com>
2026-06-08 01:44:13 +0530

    Refactor LinearSearch array declaration

--- src/Day4/LinearSearch.java
+++ src/Day4/LinearSearch.java
@@ -4,7 +4,7 @@ public class LinearSearch {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter 5 elements: ");
-        int [] n = new int [5];
+        int n[] = new int [5];
         for(int i = 0; i < n.length; i++ ){
           n[i] = sc.nextInt();
         }
