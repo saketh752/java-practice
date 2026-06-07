20df7048f3badd390de114d60af44e0bf298f519
saketh752 <sakethyadav208@gmail.com>
2026-06-08 01:45:04 +0530

    Built Mini Project 1 Student Management System

---
+++ src/MiniProject1/StudentManagementSystem.java
@@ -0,0 +1,52 @@
+package MiniProject1;
+import java.util.Scanner;
+public class StudentManagementSystem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String names[] = new String[3];
+        int marks[] = new int[3];
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("Enter Student name: ");
+            names[i] = sc.next();
+
+            System.out.println("Enter Student marks: ");
+            marks[i] = sc.nextInt();
+        }
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("Name: " + names[i] + " ");
+            System.out.println("Marks: " + marks[i] + " ");
+        }
+        System.out.println("Enter student name to search: ");
+        String searchName = sc.next();
+        int index = -1;
+        for (int i = 0; i < names.length; i++) {
+            if (names[i].equals(searchName)) {
+                index = i;
+                break;
+            }
+        }
+        if (index != -1) {
+            System.out.println("Found");
+            System.out.println(marks[index]);
+        } else {
+            System.out.println("Student Not Found");
+        }
+        int max = marks[0];
+        int maxIndex = 0;
+        for (int i = 0; i < names.length; i++) {
+            if (marks[i] > max) {
+                max = marks[i];
+                maxIndex = i;
+            }
+        }
+        System.out.println("Top Student = " + names[maxIndex]);
+        System.out.println("Highest Marks = " + max);
+        int sum = 0;
+        double average = 0;
+        for (int i = 0; i < names.length; i++) {
+            sum += marks[i];
+        }
+        average = (double) sum / marks.length;
+        System.out.println("Average Marks = " + average);
+    }
+}
